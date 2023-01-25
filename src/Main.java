410d012b9d09cba0d60680c509b58fe3025b116e
Shruti <shruti@simformsolutions.com>
2023-01-25 16:26:25 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Pattern Code");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
